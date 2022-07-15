0c9b40a334ea86068d8e6a18a7af746da8d28a5a
ioni1991 <jhonatanrojas790@gmail.comm>
2022-07-14 23:05:16 -0300

    retro

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const publicPath = path.join(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(3000,() => {
-    console.log("servidor iniciado en: http://localhost:3000");
+app.listen(3030,() => {
+    console.log("servidor iniciado en: http://localhost:3030");
 });
 
 app.get('/', (req, res) => {
